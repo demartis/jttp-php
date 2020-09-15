732b5cedce66f7bf0e2d36a568178af108300962
Riccardo De Martis <riccardo@demartis.it>
2020-09-15 16:07:34 +0200

    test: first functional tests

---
+++ tests/JttpResponseTest.php
@@ -0,0 +1,59 @@
+<?php
+
+
+use Jttp\Jttp;
+use Jttp\JttpResponse;
+use PHPUnit\Framework\TestCase;
+
+class JttpResponseTest extends TestCase
+{
+    protected $data;
+    protected $ok;
+    protected $okWithData;
+    protected $responseOk;
+    protected $responseOkWithData;
+
+    protected function setUp(): void
+    {
+        $this->data = array(
+            'book' => array(
+                'id' => 1,
+                'title' => 'foo',
+                'tags' => array(1, 3, 11),
+            ),
+        );
+        
+        $this->ok = new JttpResponse(Jttp::ok());
+        $this->okWithData = new JttpResponse(Jttp::ok($this->data));
+
+        $this->responseOk = JttpResponse::ok();
+        $this->responseOkWithData = JttpResponse::ok($this->data);
+
+    }
+
+    public function testOkHasCorrectStatus()
+    {
+        $this->assertEquals(Jttp::STATUS_SUCCESS, json_decode($this->ok->getContent(),true)['status']);
+        $this->assertTrue($this->ok->isOk());
+    }
+
+    public function testOkWithDataHasCorrectStatus()
+    {
+        $this->assertEquals(Jttp::STATUS_SUCCESS, json_decode($this->okWithData->getContent(),true)['status']);
+        $this->assertTrue($this->okWithData->isOk());
+    }
+
+
+    public function testResponseOkHasCorrectStatus()
+    {
+        $this->assertEquals(Jttp::STATUS_SUCCESS, json_decode($this->responseOk->getContent(),true)['status']);
+        $this->assertTrue($this->responseOk->isOk());
+    }
+
+
+    public function testResponseOkWithDataHasCorrectStatus()
+    {
+        $this->assertEquals(Jttp::STATUS_SUCCESS, json_decode($this->responseOkWithData->getContent(),true)['status']);
+        $this->assertTrue($this->responseOkWithData->isOk());
+    }
+}
